bc4e439f5f54a0936d805cde891154b0f62129c3
Joe Brady <joe.brady@uk.ibm.com>
2018-08-06 10:37:45 +0100

    Change match function to only require 1 matching word

--- src/components/BusinessTypeLookupFunctions.js
+++ src/components/BusinessTypeLookupFunctions.js
@@ -5,76 +5,31 @@ const suggest = (query, returnResultsArray) => {
     JSON.parse(JSON.stringify(businessTypesJSON))
   );
 
-  const checkForDisplayNameMatch = (displayName, query) => {
-    const displayNameWordArray = displayName
-      .toLowerCase()
-      .split(" ")
-      .filter(word => word !== "");
-    const queryArray = query
+  const checkForQueryMatch = (displayName, query, searchTerm) => {
+    const wordArray = displayName
       .toLowerCase()
       .split(" ")
       .filter(word => word !== "");
 
-    let matching = true;
-
-    queryArray.forEach(word => {
-      if (matching === true) {
-        matching =
-          displayNameWordArray.findIndex(entry => entry.startsWith(word)) !==
-          -1;
-      }
-    });
-    return matching;
-  };
-
-  const checkForSearchTermMatch = (searchTerm, displayName, query) => {
-    const searchTermStringArray = searchTerm
-      .toLowerCase()
-      .split(" ")
-      .filter(word => word !== "");
-
-    const displayNameStringArray = displayName
-      .toLowerCase()
-      .split(" ")
-      .filter(word => word !== "");
-
-    const queryArray = query
-      .toLowerCase()
-      .split(" ")
-      .filter(word => word !== "");
-
-    let queryWordsNotInSearchTerm = [];
-
-    queryArray.forEach(word => {
-      if (
-        searchTermStringArray.findIndex(entry => entry.startsWith(word)) === -1
-      ) {
-        queryWordsNotInSearchTerm.push(word);
-      }
-    });
-
-    // if none of the query words matched the search term, it's not a match
-    if (queryArray.length === queryWordsNotInSearchTerm.length) {
-      return false;
-      // if all of the query words matched the search term, it's a match
-    } else if (queryWordsNotInSearchTerm.length === 0) {
-      return true;
-      // if the search term is matched and there is at least one remaining word unmatched,
-      // then search the display name as well
-    } else {
-      let remainingQueryWordsMatchDisplayName = true;
-
-      queryWordsNotInSearchTerm.forEach(word => {
-        if (remainingQueryWordsMatchDisplayName === true) {
-          remainingQueryWordsMatchDisplayName =
-            displayNameStringArray.findIndex(entry =>
-              entry.startsWith(word)
-            ) !== -1;
-        }
-      });
-
-      return remainingQueryWordsMatchDisplayName;
+    if (searchTerm) {
+      wordArray.concat(
+        searchTerm
+          .toLowerCase()
+          .split(" ")
+          .filter(word => word !== "")
+      );
     }
+
+    const queryArray = query
+      .toLowerCase()
+      .split(" ")
+      .filter(word => word !== "");
+
+    let matching = queryArray.some(
+      word => wordArray.findIndex(entry => entry.startsWith(word)) !== -1
+    );
+
+    return matching;
   };
 
   let displayNameMatchArray;
@@ -91,7 +46,7 @@ const suggest = (query, returnResultsArray) => {
             .indexOf(obj["displayName"]) === pos
       )
       // check for matching words and beginnings of words
-      .filter(entry => checkForDisplayNameMatch(entry.displayName, query))
+      .filter(entry => checkForQueryMatch(entry.displayName, query))
       // remove the searchTerm field of each result
       .map(entry => {
         return { displayName: entry.displayName, searchTerm: undefined };
@@ -99,7 +54,7 @@ const suggest = (query, returnResultsArray) => {
 
     searchTermMatchArray = businessTypesArray.filter(entry =>
       // check for matching words and beginnings of words (including the displayName)
-      checkForSearchTermMatch(entry.searchTerm, entry.displayName, query)
+      checkForQueryMatch(entry.searchTerm, query, entry.displayName)
     );
 
     resultsArray = displayNameMatchArray.concat(searchTermMatchArray);
