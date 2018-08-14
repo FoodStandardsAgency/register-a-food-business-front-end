e01ce30fbfd7a9e466abff2f112dcb1d15a16cbe
Django Shelton <djangoshelton@gmail.com>
2018-08-14 12:34:32 +0100

    replace logging with standard service

---
+++ src/server/services/logging.service.js
@@ -0,0 +1,32 @@
+const EventEmitter = require("events");
+const { info, error } = require("winston");
+
+class LogEmitter extends EventEmitter {}
+
+const logEmitter = new LogEmitter();
+
+logEmitter.on("functionCall", (module, functionName) => {
+  info(`${module}: ${functionName} called`);
+});
+
+logEmitter.on("functionCallWith", (module, functionName, data) => {
+  info(`${module}: ${functionName} called with ${data}`);
+});
+
+logEmitter.on("functionSuccess", (module, functionName) => {
+  info(`${module}: ${functionName} successful`);
+});
+
+logEmitter.on("functionSuccessWith", (module, functionName, data) => {
+  info(`${module}: ${functionName} successful with ${data}`);
+});
+
+logEmitter.on("functionFail", (module, functionName, err) => {
+  error(`${module}: ${functionName} failed with: ${err}`);
+});
+
+logEmitter.on("doubleMode", (module, functionName) => {
+  info(`${module}: ${functionName}: running in double mode`);
+});
+
+module.exports = { logEmitter };
