33ca85baaac59b51d3c3b8a1019e404347332a51
Joe Brady <joe.brady@uk.ibm.com>
2018-08-17 12:42:20 +0100

    Add Babel transpiling for specific browser versions

--- client/polyfills.js
+++ client/polyfills.js
@@ -1,5 +1,1 @@
-// TODO JMB: find root cause of IE11 bug. Might not be because Object.keys is missing.
-
-// import keys from "core-js/library/fn/object/keys";
-
-// Object.prototype.keys = keys;
+import "@babel/polyfill";
