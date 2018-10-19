7c99e6a8cf5f94832518eefe2b130fd4ecfd0cca
libertyjacklin1 <liberty.jacklin@gmail.com>
2018-10-19 10:58:16 +0100

    Write tests for edit controller

---
+++ src/server/controllers/edit.controller.test.js
@@ -0,0 +1,35 @@
+describe("Edit controller", () => {
+  describe("given an answer with white space", () => {
+    it("should return the answer without whitespace", () => {});
+  });
+  describe("given an answer on this page was previously truthy and is now not part of the new answers", () => {
+    it("should not be part of the returned cumulative answers", () => {});
+  });
+  describe("given previous answers and new answers", () => {
+    it("should return cumulative answers that include the new answers", () => {});
+  });
+  describe("given that there are validation errors", () => {
+    it("should return the redirect route", () => {});
+    it("should return the errors", () => {});
+  });
+  describe("given that the cumulative edit route answers are associated with switches", () => {
+    describe("given that the current page is at the end of the edit route", () => {});
+    describe("given that the current page is NOT at the end of the edit route", () => {});
+    // TESTS FOR EDIT PATH FUNCTION
+    // describe("given that the new answers trigger the same switches as the old answers", () => {
+    //   it("should return a redirect route with the next page", () => {});
+    // });
+    // describe("given that the new answers trigger different switches to the old answers", () => {
+    //     it("should ", () => {});
+    //   });
+  });
+  describe("given that the cumulative edit route answers are NOT associated with switches", () => {
+    describe("given", () => {
+      it("should ", () => {});
+    });
+  });
+});
+
+describe("given", () => {
+  it("should ", () => {});
+});
--- src/server/routes/edit.route.js
+++ src/server/routes/edit.route.js
@@ -4,9 +4,24 @@ const { logEmitter } = require("../services/logging.service");
 const editRouter = () => {
   const router = Router();
 
-  router.get("/continue/:originator", (req, res) => {});
+  router.post("/continue/:originator", (req, res) => {
+    //step 1 click change
+    //step 2 user is sent to page that they want to edit with editMode on
+    //user changes details
+    //user presses save
+    // user's new data from that page is sent to the edit route, along with all previous/existing answers
+    // some functions are called to work out where to go next
+    const controllerResponse = editController(
+      
+    );
 
-  router.get("/back/:originator", (req, res) => {});
+    
+    // the updated user data is added to the session
+
+    
+    // user is either redirected to another page within subflow or to registration summary
+    //res.redirect(`/new/${req.session.council}/continue/:originator`)
+  });
 
   router.get("/:target", (req, res) => {
     logEmitter.emit("functionCall", "Routes", "/edit/:target route");
