469091508d411919c8d059f30cb7013e92f106e5
nick-casadei <nicholas.casadei@ibm.com>
2018-12-12 11:27:32 +0000

    copy changes ref 10

--- pages/contact-representative.js
+++ pages/contact-representative.js
@@ -96,7 +96,7 @@ const ContactRepresentative = props => {
               }}
               id="contact_representative_email"
               hint={[
-                "Your email address will be used to update you regarding policy or legal changes that could affect your food business."
+                "We will use your email to keep you informed of any policy or legal changes that could affect your food business."
               ]}
               meta={{
                 touched: true,
