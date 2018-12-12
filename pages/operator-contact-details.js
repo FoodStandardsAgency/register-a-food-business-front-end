16f5c5abaf7e412450fbe07c332ee44071535c6b
nick-casadei <nicholas.casadei@ibm.com>
2018-12-12 17:01:22 +0000

    update to copy change ref 1

--- pages/operator-contact-details.js
+++ pages/operator-contact-details.js
@@ -24,7 +24,7 @@ const OperatorContactDetails = props => (
 
     <HiddenTextAccessible summaryText={"What is a food business operator?"}>
       <Paragraph mb={0}>
-        The operator is the person or people, charity or company who makes the
+        The operator is the person or persons, charity or company who makes the
         decisions about the food business. They decide what it serves and how it
         operates.
       </Paragraph>
