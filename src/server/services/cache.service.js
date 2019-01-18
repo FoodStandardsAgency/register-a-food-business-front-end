01dc5513e930dcc12efba4c87d12c0f0dbee4eaf
Michal Rak <michal.rak@civica.co.uk>
2019-01-18 11:55:22 +0000

    created cache service

---
+++ src/server/services/cache.service.js
@@ -0,0 +1,59 @@
+const NodeCache = require("node-cache");
+const { logEmitter } = require("./logging.service");
+
+// Enforce one variable per Cache object
+const key = "x";
+
+class Cache {
+  /**
+   * Class constructor
+   * @param {int} stdTTL Standard TimeToLive in seconds (0 = unlimited)
+   * @param {boolean} [deleteOnExpire=true] Whether cache variables are to be deleted upon expiry
+   * @param {boolean} [autoRetrieveOnExpiry=false] Whether to obtain and cache value upon expiry
+   * @param {function} [getValue={return null}] Function to use to obtain value if not in cache
+   */
+  constructor(
+    stdTTL,
+    deleteOnExpire = true,
+    autoRetrieveOnExpiry = false,
+    getValue = () => {
+      return null;
+    }
+  ) {
+    this.getValue = getValue;
+    this.cache = new NodeCache({
+      stdTTL,
+      checkperiod: stdTTL * 0.2,
+      deleteOnExpire
+    });
+
+    if (autoRetrieveOnExpiry) {
+      this.cache.on("expired", () => {
+        return getValue().then(result => {
+          this.cache.set(key, result);
+        });
+      });
+    }
+  }
+
+  /**
+   * Retrieves value of the Cache object
+   * If no value is found in cache, object's getValue function is executed and result automatically saved to cache
+   * @returns {any} Cache object's value
+   */
+  get() {
+    logEmitter.emit("functionCall", "cache.service", "get");
+
+    const value = this.cache.get(key);
+    if (value) {
+      return Promise.resolve(value);
+    }
+
+    return getValue().then(result => {
+      this.cache.set(key, result);
+      return result;
+    });
+  }
+}
+
+module.exports = { Cache };
