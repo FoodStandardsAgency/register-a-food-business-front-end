135fdb1fdf8bf2074a17ae2a20007005ae58c38a
Michal Rak <michal.rak@civica.co.uk>
2019-01-28 18:13:54 +0000

    add new test for cache auto retrieve

--- src/server/services/cache.service.test.js
+++ src/server/services/cache.service.test.js
@@ -36,4 +36,41 @@ describe("cache.service get()", () => {
       expect(result).toEqual(obj1);
     });
   });
+  describe("given deleteOnExpire is enabled", () => {
+    const ttl = 0.6;
+    const deleteOnExpire = true;
+    const waitTimeForCacheToExpire = 700;
+    describe("given autoRetrieveOnExpiry is enabled", () => {
+      const waitTimeForValueToAutoRetrieve = 400;
+      const autoRetrieveOnExpiry = true;
+      let mockResult = obj1;
+      const cache = new Cache(ttl, deleteOnExpire, autoRetrieveOnExpiry, () => {
+        return Promise.resolve(mockResult);
+      });
+
+      const delay = t => new Promise(resolve => setTimeout(resolve, t));
+      let result;
+      beforeEach(() => {
+        return cache.get().then(() => {
+          mockResult = obj2;
+          return delay(waitTimeForCacheToExpire).then(() => {
+            if (cache.cache.get("x") === undefined) {
+              return delay(waitTimeForValueToAutoRetrieve).then(() => {
+                return cache.cache.get("x", (err, value) => {
+                  result = Promise.resolve(value);
+                });
+              });
+            } else {
+              return cache.cache.get("x", (err, value) => {
+                result = Promise.resolve(value);
+              });
+            }
+          });
+        });
+      });
+      it("should return new mockResult value as cache expired", () => {
+        return result.then(r => expect(r).toEqual(obj2));
+      });
+    });
+  });
 });
