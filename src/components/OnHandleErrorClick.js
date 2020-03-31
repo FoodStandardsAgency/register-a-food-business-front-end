16e264abdf189f5833d7cf6e1a23d071775ea549
Josh <joshua.perry@civica.co.uk>
2020-03-31 15:48:50 +0000

    bug fix

--- src/components/OnHandleErrorClick.js
+++ src/components/OnHandleErrorClick.js
@@ -1,8 +1,16 @@
 const OnHandleErrorClick = targetName => {
-  const targetElement =
-    document.getElementsByName(targetName)[0] ||
-    document.getElementById(targetName);
-  targetElement.scrollIntoView();
+  if (targetName === "establishment_opening_status") {
+    let targetTradingDate =
+      document.getElementsByName(targetName)[0] ||
+      document.getElementById(targetName) ||
+      document.getElementById("establishment_opening_date");
+    targetTradingDate.scrollIntoView();
+  } else {
+    let targetElement =
+      document.getElementsByName(targetName)[0] ||
+      document.getElementById(targetName);
+    targetElement.scrollIntoView();
+  }
 };
 
 export default OnHandleErrorClick;
