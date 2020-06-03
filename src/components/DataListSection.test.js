dbbf807b08d65dc625a2310238cc443333f2a402
Josh <joshua.perry@civica.co.uk>
2020-06-03 08:13:28 +0000

    Add unit test

--- src/components/DataListSection.test.js
+++ src/components/DataListSection.test.js
@@ -4,6 +4,25 @@ import { Paragraph } from "govuk-react";
 import { mount } from "enzyme";
 import { act } from "react-dom/test-utils"; // ES6
 
+describe("given browser is IE", () => {
+  it("renders DataListSection", async () => {
+    let wrapper;
+
+    await act(async () => {
+      wrapper = mount(
+        <BusinessTypeLookup
+          validatorErrors
+          cumulativeFullAnswers
+          browser="IE"
+        />
+      );
+      wrapper.setState({ renderAutoCompleteSection: true });
+      wrapper.update();
+    });
+
+    expect(wrapper.find(DataListSection)).toHaveLength(1);
+  });
+});
 describe("given JavaScript is disabled", () => {
   describe("when browser is not Safari", () => {
     it("renders DataListSection", async () => {
