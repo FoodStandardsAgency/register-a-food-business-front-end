a11e7015573eee16ffb23fd6b31c6870f4141544
Josh <joshua.perry@civiva.co.uk>
2020-07-16 09:10:35 +0000

    Ensure failed with produces a message

--- src/server/services/logging.service.js
+++ src/server/services/logging.service.js
@@ -75,7 +75,7 @@ logEmitter.on(FUNCTION_SUCCESS_WITH, (module, functionName, data) => {
 logEmitter.on(
   FUNCTION_FAIL,
   (module, functionName, err = { message: null }) => {
-    let message = `${module}: ${functionName} failed with: ${err.message}`;
+    let message = `${module}: ${functionName} failed with: ${err.message || err}`;
     logStuff(message, {}, "error");
   }
 );
