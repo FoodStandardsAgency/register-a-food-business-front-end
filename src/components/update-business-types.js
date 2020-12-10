807f2c3ab592a42428fd4263930a9b31c007b37e
Joshua Perry <joshua.perry@civica.co.uk>
2020-12-10 15:13:58 +0000

    Update function to include missing business-types

--- src/components/update-business-types.js
+++ src/components/update-business-types.js
@@ -9,7 +9,9 @@ const v1BusinessTypesMapping = {
   "Market stall with permanent location": "Market stalls with permanent pitch",
   "Restaurant, cafe, canteen, or fast food restaurant":
     "Restaurant, cafe, canteen or fast food",
-  "Hostel or bed & breakfast": "Hostel or bed and breakfast"
+  "Hostel or bed & breakfast": "Hostel or bed and breakfast ",
+  "Commercial Bakery": "Commercial bakery",
+  "Childcare, nursery or play group": "Childcarer, nursery or play group"
 };
 
 const updateBusinessTypesForAutocomplete = async () => {
@@ -79,6 +81,27 @@ const updateBusinessTypesForAutocomplete = async () => {
         return console.log(err);
       }
       console.log(`SUCCESS: ${en_filename} updated.`);
+    }
+  );
+
+  function getDistinctBusinessTypes(transformedBusinessTypes) {
+    const distinctBusinessTypes = [];
+    transformedBusinessTypes.forEach((businessType) => {
+      distinctBusinessTypes.push(businessType.displayName);
+    });
+    const distinctBusinessTypesUnique = [...new Set(distinctBusinessTypes)];
+    return distinctBusinessTypesUnique;
+  }
+
+  const en__distinct_filename = "./src/components/distinct-business-types.json";
+  fs.writeFile(
+    en__distinct_filename,
+    JSON.stringify(getDistinctBusinessTypes(transformedBusinessTypeArray_en)),
+    (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(`SUCCESS: ${en__distinct_filename} updated`);
     }
   );
 };
