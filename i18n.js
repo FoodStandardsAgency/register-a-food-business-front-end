e2ab0be718413de2a4cde3233dad3ab2401116e7
David Kasali <david.kasali@civica.co.uk>
2020-12-10 14:13:38 +0000

    i18n file edit

--- i18n.js
+++ i18n.js
@@ -2,7 +2,6 @@ const NextI18Next = require("next-i18next").default;
 const path = require("path");
 
 module.exports = new NextI18Next({
-  defaultLanguage: "en",
   otherLanguages: ["cy"],
   localePath: path.resolve("./public/static/locales"),
   keySeparator: false,
