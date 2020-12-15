7f9a6f76b104160dc1e4e1350e38e44054954182
Paul Burnett <paul.burnett@civica.co.uk>
2020-12-15 13:23:52 +0000

    Update tests

--- src/components/PageTitles.test.js
+++ src/components/PageTitles.test.js
@@ -2,18 +2,18 @@ import { PageTitles } from "../components";
 
 describe("PageTitles", () => {
   it("returns the default title when URL not recognised", () => {
-    const title = PageTitles.getUrlPageTitle("not/recognised");
+    const title = PageTitles.getUrlPageTitle("not/recognised", "en");
     expect(title).toBe(PageTitles.defaultPageTitle);
   });
 
   it("returns the default title when URL empty", () => {
-    const title = PageTitles.getUrlPageTitle("/");
+    const title = PageTitles.getUrlPageTitle("/", "en");
     expect(title).toBe(PageTitles.defaultPageTitle);
   });
 
   it("returns the relevant page title with prefix for URL", () => {
     const page = "business-type";
-    const title = PageTitles.getUrlPageTitle(`new/council/${page}`);
+    const title = PageTitles.getUrlPageTitle(`new/council/${page}`, "en");
     expect(title).toBe(`${PageTitles.prefix} - ${PageTitles.pageTitles[page]}`);
   });
 });
