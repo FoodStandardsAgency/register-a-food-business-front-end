facc5bb5e544b15d87bf4b014d9971d9c42bb2be
David Kasali <david.kasali@civica.co.uk>
2020-12-16 14:34:44 +0000

    fsa header tests update

--- src/components/FsaHeader.test.js
+++ src/components/FsaHeader.test.js
@@ -1,23 +1,23 @@
 import FsaHeader from "../components/FsaHeader";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../../i18nForTests.js";
 
 describe("<FsaHeader />", () => {
   it("renders without crashing", () => {
-    const wrapper = shallow(<FsaHeader />);
+    const wrapper = mount(<FsaHeader />);
     expect(wrapper.length).toBe(1);
   });
 
   it("renders the cookie banner when acceptAllCookies is undefined", () => {
-    const wrapper = shallow(<FsaHeader />);
+    const wrapper = mount(<FsaHeader />);
     const cookieBanner = wrapper.find("CookieBanner");
     expect(cookieBanner.length).toBe(1);
   });
 
   describe("given acceptAllCookies = 'true'", () => {
     it("does not render the cookie banner", () => {
-      const wrapper = shallow(<FsaHeader acceptAllCookies="true" />);
+      const wrapper = mount(<FsaHeader acceptAllCookies="true" />);
       const cookieBanner = wrapper.find("CookieBanner");
       expect(cookieBanner.length).toBe(0);
     });
@@ -25,7 +25,7 @@ describe("<FsaHeader />", () => {
 
   describe("given acceptAllCookies = 'false'", () => {
     it("does not render the cookie banner", () => {
-      const wrapper = shallow(<FsaHeader acceptAllCookies="false" />);
+      const wrapper = mount(<FsaHeader acceptAllCookies="false" />);
       const cookieBanner = wrapper.find("CookieBanner");
       expect(cookieBanner.length).toBe(0);
     });
