62cb16009c23cbf432732d1bdab080f36cab637b
Paul Burnett <paul.burnett@civica.co.uk>
2021-01-18 14:54:41 +0000

    Upversion for 11.0.0.2

--- src/components/BusinessTypeLookup.js
+++ src/components/BusinessTypeLookup.js
@@ -42,7 +42,7 @@ const lineHeight = {
   padding: 0
 };
 
-// 01/12/2020 - comment needed due to cloudflare caching next.js files
+// 18/01/2020 - comment needed due to cloudflare caching next.js files
 export class BusinessTypeLookup extends React.Component {
   constructor(props) {
     super(props);
