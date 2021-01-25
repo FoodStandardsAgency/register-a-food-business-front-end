e6305ec685ab57842199ea07aee8bc00968a1104
David Kasali <david.kasali@civica.co.uk>
2021-01-25 12:20:26 +0000

    edit to include label based on where called from

--- src/components/AddressHelp.js
+++ src/components/AddressHelp.js
@@ -7,27 +7,58 @@ import { operatorTypeEnum } from "@slice-and-dice/register-a-food-business-valid
 
 const AddressHelp = (props) => {
   return props.role === operatorTypeEnum.PARTNERSHIP.key ? (
-    <React.Fragment>
-      <Heading as="h1" size="LARGE">
-        <label for={props.id}>
+    props.id ? (
+      <React.Fragment>
+        <Heading as="h1" size="LARGE">
+          <label for={props.id}>
+            {props.t("What is the partnership contact's address?")}
+          </label>
+        </Heading>
+        <ContentItem.B_30_15>
+          <HintText>
+            {props.t(
+              "Partnership address is the contact address for the partner who is the main point of contact."
+            )}
+          </HintText>
+        </ContentItem.B_30_15>
+        <PartnershipDescription />
+      </React.Fragment>
+    ) : (
+      <React.Fragment>
+        <Heading as="h1" size="LARGE">
           {props.t("What is the partnership contact's address?")}
-        </label>
-      </Heading>
-      <ContentItem.B_30_15>
-        <HintText>
-          {props.t(
-            "Partnership address is the contact address for the partner who is the main point of contact."
-          )}
-        </HintText>
-      </ContentItem.B_30_15>
-      <PartnershipDescription />
-    </React.Fragment>
-  ) : (
+        </Heading>
+        <ContentItem.B_30_15>
+          <HintText>
+            {props.t(
+              "Partnership address is the contact address for the partner who is the main point of contact."
+            )}
+          </HintText>
+        </ContentItem.B_30_15>
+        <PartnershipDescription />
+      </React.Fragment>
+    )
+  ) : props.id ? (
     <React.Fragment>
       <Heading as="h1" size="LARGE">
         <label for={props.id}>
           {props.t("What is the operator's address?")}
         </label>
+      </Heading>
+
+      <ContentItem.B_30_15>
+        <HintText>
+          {props.t(
+            "Operator address is the contact address for the operator. For example home address for a sole trader or headquarters address for a limited company."
+          )}
+        </HintText>
+      </ContentItem.B_30_15>
+      <OperatorDescription />
+    </React.Fragment>
+  ) : (
+    <React.Fragment>
+      <Heading as="h1" size="LARGE">
+        {props.t("What is the operator's address?")}
       </Heading>
 
       <ContentItem.B_30_15>
