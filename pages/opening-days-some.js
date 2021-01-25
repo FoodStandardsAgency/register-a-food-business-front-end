e2ded852a7b7b40c58f85f914cf77ad3d48530bf
David Kasali <david.kasali@civica.co.uk>
2021-01-25 14:28:52 +0000

    add fieldset and legend

--- pages/opening-days-some.js
+++ pages/opening-days-some.js
@@ -8,7 +8,13 @@ import {
   OnHandleErrorClick,
   PostForm
 } from "../src/components";
-import { Heading, Checkbox, MultiChoice, Paragraph } from "govuk-react";
+import {
+  Fieldset,
+  Heading,
+  Checkbox,
+  MultiChoice,
+  Paragraph
+} from "govuk-react";
 import PropTypes from "prop-types";
 import { withTranslation } from "../i18n";
 
@@ -20,81 +26,85 @@ const OpeningSomeDays = (props) => (
         validatorErrors={props.validatorErrors}
         onHandleErrorClick={OnHandleErrorClick}
       />
-      <Heading as="h1" size="LARGE">
-        {props.t("Opening days")}
-      </Heading>
-      <Paragraph>
-        {props.t(
-          "What days will this establishment be producing or serving food?"
-        )}
-      </Paragraph>
-      <Paragraph>{props.t("Select all that apply")}</Paragraph>
-      <ContentItem.B_30_15>
-        <MultiChoice
-          label=""
-          meta={{
-            touched: true,
-            error: props.t(props.validatorErrors.opening_days_some)
-          }}
-        >
-          <Checkbox
-            name="opening_day_monday"
-            id="opening_day_monday"
-            value="Monday"
-            defaultChecked={props.cumulativeFullAnswers.opening_day_monday}
+      <Fieldset>
+        <Fieldset.Legend>
+          <Heading as="h1" size="LARGE">
+            {props.t("Opening days")}
+          </Heading>
+        </Fieldset.Legend>
+        <Paragraph>
+          {props.t(
+            "What days will this establishment be producing or serving food?"
+          )}
+        </Paragraph>
+        <Paragraph>{props.t("Select all that apply")}</Paragraph>
+        <ContentItem.B_30_15>
+          <MultiChoice
+            label=""
+            meta={{
+              touched: true,
+              error: props.t(props.validatorErrors.opening_days_some)
+            }}
           >
-            {props.t("Monday")}
-          </Checkbox>
-          <Checkbox
-            name="opening_day_tuesday"
-            id="opening_day_tuesday"
-            value="Tuesday"
-            defaultChecked={props.cumulativeFullAnswers.opening_day_tuesday}
-          >
-            {props.t("Tuesday")}
-          </Checkbox>
-          <Checkbox
-            name="opening_day_wednesday"
-            id="opening_day_wednesday"
-            value="Wednesday"
-            defaultChecked={props.cumulativeFullAnswers.opening_day_wednesday}
-          >
-            {props.t("Wednesday")}
-          </Checkbox>
-          <Checkbox
-            name="opening_day_thursday"
-            id="opening_day_thursday"
-            value="Thursday"
-            defaultChecked={props.cumulativeFullAnswers.opening_day_thursday}
-          >
-            {props.t("Thursday")}
-          </Checkbox>
-          <Checkbox
-            name="opening_day_friday"
-            id="opening_day_friday"
-            value="Friday"
-            defaultChecked={props.cumulativeFullAnswers.opening_day_friday}
-          >
-            {props.t("Friday")}
-          </Checkbox>
-          <Checkbox
-            name="opening_day_saturday"
-            id="opening_day_saturday"
-            value="Saturday"
-            defaultChecked={props.cumulativeFullAnswers.opening_day_saturday}
-          >
-            {props.t("Saturday")}
-          </Checkbox>
-          <Checkbox
-            name="opening_day_sunday"
-            id="opening_day_sunday"
-            value="Sunday"
-            defaultChecked={props.cumulativeFullAnswers.opening_day_sunday}
-          >
-            {props.t("Sunday")}
-          </Checkbox>
-        </MultiChoice>
-      </ContentItem.B_30_15>
+            <Checkbox
+              name="opening_day_monday"
+              id="opening_day_monday"
+              value="Monday"
+              defaultChecked={props.cumulativeFullAnswers.opening_day_monday}
+            >
+              {props.t("Monday")}
+            </Checkbox>
+            <Checkbox
+              name="opening_day_tuesday"
+              id="opening_day_tuesday"
+              value="Tuesday"
+              defaultChecked={props.cumulativeFullAnswers.opening_day_tuesday}
+            >
+              {props.t("Tuesday")}
+            </Checkbox>
+            <Checkbox
+              name="opening_day_wednesday"
+              id="opening_day_wednesday"
+              value="Wednesday"
+              defaultChecked={props.cumulativeFullAnswers.opening_day_wednesday}
+            >
+              {props.t("Wednesday")}
+            </Checkbox>
+            <Checkbox
+              name="opening_day_thursday"
+              id="opening_day_thursday"
+              value="Thursday"
+              defaultChecked={props.cumulativeFullAnswers.opening_day_thursday}
+            >
+              {props.t("Thursday")}
+            </Checkbox>
+            <Checkbox
+              name="opening_day_friday"
+              id="opening_day_friday"
+              value="Friday"
+              defaultChecked={props.cumulativeFullAnswers.opening_day_friday}
+            >
+              {props.t("Friday")}
+            </Checkbox>
+            <Checkbox
+              name="opening_day_saturday"
+              id="opening_day_saturday"
+              value="Saturday"
+              defaultChecked={props.cumulativeFullAnswers.opening_day_saturday}
+            >
+              {props.t("Saturday")}
+            </Checkbox>
+            <Checkbox
+              name="opening_day_sunday"
+              id="opening_day_sunday"
+              value="Sunday"
+              defaultChecked={props.cumulativeFullAnswers.opening_day_sunday}
+            >
+              {props.t("Sunday")}
+            </Checkbox>
+          </MultiChoice>
+        </ContentItem.B_30_15>
+      </Fieldset>
       <ContinueButton {...props} />
     </PostForm>
   </FsaLayout>
