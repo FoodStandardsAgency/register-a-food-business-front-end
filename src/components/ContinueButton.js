eb552f62a1701160b47e8e027036500477bc2136
David Kasali <david.kasali@civica.co.uk>
2021-02-12 09:44:57 +0000

    format and continue button test update

--- src/components/ContinueButton.js
+++ src/components/ContinueButton.js
@@ -13,8 +13,11 @@ const ContinueButton = (props) => (
     disabled={props.disabled}
     id="continue-button"
     type="submit"
-    icon={props.type === "begin" ? <ButtonArrow aria-hidden="true"
-    focusable="false" /> : null}
+    icon={
+      props.type === "begin" ? (
+        <ButtonArrow aria-hidden="true" focusable="false" />
+      ) : null
+    }
     start={props.type === "begin"}
   >
     {props.type === "begin"
--- src/components/ContinueButton.test.js
+++ src/components/ContinueButton.test.js
@@ -54,6 +54,10 @@ describe("<ContinueButton />", () => {
       const GovUkButton = wrapper.find("#continue-button").at(0);
       expect(GovUkButton.props().start).toBe(true);
     });
+    it("contains a <ButtonArrow> component", () => {
+      const buttonArrow = wrapper.find("ButtonArrow");
+      expect(buttonArrow.length).toBe(1);
+    });
   });
 
   describe("given that edit mode is true", () => {
