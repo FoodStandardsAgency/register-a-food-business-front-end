396a8a1f79c12bc011a5b5a178f7eee779c82dfa
David Kasali <david.kasali@civica.co.uk>
2021-03-09 16:58:58 +0000

    test update

--- src/components/PageTitles.test.js
+++ src/components/PageTitles.test.js
@@ -1,19 +1,23 @@
 import { PageTitles } from "../components";
 
+const testValidatorErrors = {};
+
+const testAllValidationErrors = {};
+
 describe("PageTitles", () => {
   it("returns the default title when URL not recognised", () => {
-    const title = PageTitles.getUrlPageTitle("not/recognised");
+    const title = PageTitles.getUrlPageTitle("not/recognised", testValidatorErrors, testAllValidationErrors);
     expect(title).toBe(PageTitles.defaultPageTitle);
   });
 
   it("returns the default title when URL empty", () => {
-    const title = PageTitles.getUrlPageTitle("/");
+    const title = PageTitles.getUrlPageTitle("/", testValidatorErrors, testAllValidationErrors);
     expect(title).toBe(PageTitles.defaultPageTitle);
   });
 
   it("returns the relevant page title with prefix for URL", () => {
     const page = "business-type";
-    const title = PageTitles.getUrlPageTitle(`new/council/${page}`);
+    const title = PageTitles.getUrlPageTitle(`new/council/${page}`, testValidatorErrors, testAllValidationErrors);
     expect(title).toBe(`${PageTitles.prefix} - ${PageTitles.pageTitles[page]}`);
   });
 });
