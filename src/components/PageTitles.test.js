74cf6adaa399fec131e48d021a166720fc13e413
David Kasali <david.kasali@civica.co.uk>
2021-03-12 10:42:22 +0000

    unit test update

--- src/components/PageTitles.test.js
+++ src/components/PageTitles.test.js
@@ -2,9 +2,20 @@ import { PageTitles } from "../components";
 
 const testValidatorErrors = {};
 
+const testValidatorErrorsPopulated = {test: 'test'};
+
 const testAllValidationErrors = {};
 
 describe("PageTitles", () => {
+  it("returns the default error title when URL not recognised when validator errors is populated", () => {
+    const title = PageTitles.getUrlPageTitle(
+      "not/recognised",
+      testValidatorErrorsPopulated,
+      testAllValidationErrors
+    );
+    expect(title).toBe(`Error ${PageTitles.defaultPageTitle}`);
+  });
+
   it("returns the default title when URL not recognised", () => {
     const title = PageTitles.getUrlPageTitle(
       "not/recognised",
@@ -14,6 +25,15 @@ describe("PageTitles", () => {
     expect(title).toBe(PageTitles.defaultPageTitle);
   });
 
+  it("returns the default error title when URL empty when validator errors is populated", () => {
+    const title = PageTitles.getUrlPageTitle(
+      "/",
+      testValidatorErrorsPopulated,
+      testAllValidationErrors
+    );
+    expect(title).toBe(`Error ${PageTitles.defaultPageTitle}`);
+  });
+
   it("returns the default title when URL empty", () => {
     const title = PageTitles.getUrlPageTitle(
       "/",
@@ -21,6 +41,16 @@ describe("PageTitles", () => {
       testAllValidationErrors
     );
     expect(title).toBe(PageTitles.defaultPageTitle);
+  });
+
+  it("returns the relevant error page title with prefix for URL when validator errors is populated", () => {
+    const page = "business-type";
+    const title = PageTitles.getUrlPageTitle(
+      `new/council/${page}`,
+      testValidatorErrorsPopulated,
+      testAllValidationErrors
+    );
+    expect(title).toBe(`Error ${PageTitles.prefix} - ${PageTitles.pageTitles[page]}`);
   });
 
   it("returns the relevant page title with prefix for URL", () => {
