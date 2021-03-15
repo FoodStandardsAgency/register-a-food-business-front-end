6012d20deef04768ca6444ff13eedead1097f3a9
Paul Burnett <paul.burnett@civica.co.uk>
2021-03-15 10:35:52 +0000

    Use latest govuk-react version

--- src/components/OpeningDate.js
+++ src/components/OpeningDate.js
@@ -7,11 +7,10 @@ import {
   HiddenTextAccessible,
   PostForm
 } from "./index";
-import { Heading, Paragraph, HintText } from "@slice-and-dice/govuk-react";
+import { Heading, Paragraph, HintText, DateField } from "@slice-and-dice/govuk-react";
 import moment from "moment";
 import PropTypes from "prop-types";
 import { withTranslation } from "../../i18n.js";
-import DateField from "@slice-and-dice/govuk-react-date-field";
 
 const OpeningDate = (props) => {
   return (
