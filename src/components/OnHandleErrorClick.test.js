6ac8ba1a9165ce448367dacfb83815c3b25e244d
David Kasali <david.kasali@civica.co.uk>
2021-03-30 11:01:32 +0000

    test update

--- src/components/OnHandleErrorClick.test.js
+++ src/components/OnHandleErrorClick.test.js
@@ -1,6 +1,7 @@
 import OnHandleErrorClick from "../components/OnHandleErrorClick";
 
 const scrollIntoView = jest.fn();
+const focus = jest.fn();
 document.getElementsByName = jest.fn(() => {
   return [{ scrollIntoView: scrollIntoView, focus: focus }];
 });
@@ -11,5 +12,6 @@ describe("On Handle Error Click function", () => {
     OnHandleErrorClick(targetName);
     expect(document.getElementsByName).toHaveBeenCalledWith(targetName);
     expect(scrollIntoView).toHaveBeenCalled();
+    expect(focus).toHaveBeenCalled();
   });
 });
