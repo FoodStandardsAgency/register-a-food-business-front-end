579552c27e087b974bb8f2cf41dded71574fc80b
jkaur2303 <jaswinder.kaur@civica.co.uk>
2021-10-08 07:34:09 +0000

    wip - added validator error function to testHelper.

--- testHelpers.js
+++ testHelpers.js
@@ -1,35 +1,44 @@
-const cheerio = require('cheerio')
-const nunjucks = require('nunjucks')
-const i18n = require('i18n');
-const { configureAxe } = require('jest-axe')
+const cheerio = require("cheerio");
+const nunjucks = require("nunjucks");
+const i18n = require("i18n");
+const { configureAxe } = require("jest-axe");
 
 // For accessibility testing
 const axe = configureAxe({
   rules: {
-    'skip-link': { enabled: false },
+    "skip-link": { enabled: false },
     region: { enabled: false }
   }
-})
+});
 
-module.exports = axe
+module.exports = axe;
 
 i18n.configure({
-    locales: ['en', 'cy'],
-    fallbacks: { nl: 'de' },
-    //directory: './locales',
-    register: global,
-
+  locales: ["en", "cy"],
+  fallbacks: { nl: "de" },
+  //directory: './locales',
+  register: global,
 
   // where to store json files - defaults to './locales'
   directory: __dirname + "/public/static/locales"
 });
 
-const env = nunjucks.configure(["node_modules/govuk-frontend/", "pages", "components"], {
+const env = nunjucks.configure(
+  ["node_modules/govuk-frontend/", "pages", "components"],
+  {
     trimBlocks: true,
     lstripBlocks: true
-  })
+  }
+);
 env.addGlobal("__", i18n.__);
 env.addFilter("t", i18n.__);
+
+env.addFilter("selectValidationErrors", (validationErrors, language) =>
+  Object.entries(validationErrors).map(([k, v]) => ({
+    text: i18n.__({ phrase: v, locale: language }),
+    href: "#" + k
+  }))
+);
 
 /**
  * Render a page for testing
@@ -37,41 +46,45 @@ env.addFilter("t", i18n.__);
  * @param {string} params parameters that are used in the component macro
  * @returns {function} returns cheerio (jQuery) instance of the macro for easy DOM querying
  */
- function renderPage (pageName, params) {
-    if (typeof params === 'undefined') {
-      throw new Error('Parameters passed to `render` should be an object but are undefined')
-    }
-  
-    const pageParams = JSON.stringify(params, null, 2)
-  
-    const output = nunjucks.render(pageName + ".njk", pageParams);
-    return cheerio.load(output)
+function renderPage(pageName, params) {
+  if (typeof params === "undefined") {
+    throw new Error(
+      "Parameters passed to `render` should be an object but are undefined"
+    );
   }
 
-  /**
+  const pageParams = JSON.stringify(params, null, 2);
+
+  const output = nunjucks.render(pageName + ".njk", pageParams);
+  return cheerio.load(output);
+}
+
+/**
  * Render a page for testing
  * @param {string} pageName
  * @param {string} params parameters that are used in the component macro
  * @param {any} children any child components or text, pass the children to the macro
  * @returns {function} returns cheerio (jQuery) instance of the macro for easy DOM querying
  */
-  function renderComponent (pageName, params, children = false) {
-    if (typeof params === 'undefined') {
-      throw new Error('Parameters passed to `render` should be an object but are undefined')
-    }
-  
-    const macroParams = JSON.stringify(params, null, 2)
-  
-    let macroString = `{%- from "pages/${pageName}.njk" import ${pageName} -%}`
-  
-    if (children) {
-      macroString += `{%- call ${pageName}(${macroParams}) -%}${children}{%- endcall -%}`
-    } else {
-      macroString += `{{- ${pageName}(${macroParams}) -}}`
-    }
-  
-    const output = nunjucks.renderString(macroString)
-    return cheerio.load(output)
+function renderComponent(pageName, params, children = false) {
+  if (typeof params === "undefined") {
+    throw new Error(
+      "Parameters passed to `render` should be an object but are undefined"
+    );
   }
 
-  module.exports = { axe, renderPage, renderComponent }
+  const macroParams = JSON.stringify(params, null, 2);
+
+  let macroString = `{%- from "pages/${pageName}.njk" import ${pageName} -%}`;
+
+  if (children) {
+    macroString += `{%- call ${pageName}(${macroParams}) -%}${children}{%- endcall -%}`;
+  } else {
+    macroString += `{{- ${pageName}(${macroParams}) -}}`;
+  }
+
+  const output = nunjucks.renderString(macroString);
+  return cheerio.load(output);
+}
+
+module.exports = { axe, renderPage, renderComponent };
