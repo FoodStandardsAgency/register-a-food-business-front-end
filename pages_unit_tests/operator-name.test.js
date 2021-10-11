f590a1db18226ccb342474ae907a5ef51ad652d1
jkaur2303 <jaswinder.kaur@civica.co.uk>
2021-10-11 17:27:45 +0000

    test page - will need amending

--- pages_unit_tests/operator-name.test.js
+++ pages_unit_tests/operator-name.test.js
@@ -1,35 +1,55 @@
-const { axe, renderPage, renderComponent } = require("../testHelpers");
+const { axe, renderPage } = require("../testHelpers");
 
-describe("Page heading title should be 'What is the operator's name ?'", () => {
-  it("renders without crashing", () => {
-    expect.stringContaining("What is the operator's name ?");
-  });
+const props = {
+  validatorErrors: {},
+  cumulativeFullAnswers: {
+    operator_first_name: "test name1",
+    operator_last_name: "test name2"
+  },
+  language: "en"
+};
 
+describe("Operator-Name", () => {
   it("It should pass accessibility tests", async () => {
     const $ = renderPage("operator-name", "language: 'en'");
     const results = await axe($.html());
     expect(results).toHaveNoViolations();
   });
 
-  describe("It should have operator first name input field", () => {
-    it("renders", () => {
-      const $ = renderPage("operator-name", "language: 'en'");
+  it("renders without crashing", () => {
+    const $ = renderPage("operator-name", props);
+
+    const $mainHeading = $("h1");
+    expect($mainHeading.text().trim()).toEqual("What is the operator's name?");
+  });
+
+  describe("It should have correct input fields", () => {
+    it("renders operator first name correctly", () => {
+      const $ = renderPage("operator-name", props);
       const $firstname = $("#operator_first_name");
       expect($firstname.get(0).attribs.name).toBe("operator_first_name");
     });
 
-    it("gets given the correct error prop when first name is set", () => {
+    it("renders operator last name correctly", () => {
       const $ = renderPage("operator-name", "language: 'en'");
-      const $firstname = $("#operator_first_name");
-      //const $ = renderComponent("operator-name", props.validatorErrors, true);
-    });
-  });
-
-  describe("It should have operator last name input field", () => {
-    it("renders", () => {
-      const $ = renderPage("operator-name", "language: 'en'");
+      const $ = renderPage("establishment-address-type", props);
       const $firstname = $("#operator_last_name");
       expect($firstname.get(0).attribs.name).toBe("operator_last_name");
     });
+  });
+
+  it("renders the correct error", async () => {
+    const $ = renderPage("operator-name", {
+      language: "cy",
+      validatorErrors: {
+        operator_first_name: "test name1",
+        operator_last_name: "test name2"
+      }
+    });
+
+    const $pageErrors = $(".govuk-error-summary__list a");
+    expect($pageErrors.length).toBe(2);
+    expect($pageErrors.contents().get(0).data).toBe("test name1");
+    expect($pageErrors.contents().get(1).data).toBe("test name2");
   });
 });
