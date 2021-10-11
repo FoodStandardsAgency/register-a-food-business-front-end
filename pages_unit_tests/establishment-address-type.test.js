edfe895bbd961c92447ddb41897ceb04dabbb26b
Simon Thomas <simon.thomas@civica.co.uk>
2021-10-11 14:45:16 +0100

    standardise tests

--- pages_unit_tests/establishment-address-type.test.js
+++ pages_unit_tests/establishment-address-type.test.js
@@ -1,68 +1,71 @@
-const establishmentTradingName = require("../pages/establishment-address-type.njk");
 const { axe, renderPage } = require("../testHelpers");
 
 describe("Establishment-Address-Type", () => {
   it("renders without crashing", () => {
-    const $ = renderPage("establishment-address-type", "language: 'en'");
+    const $ = renderPage("establishment-address-type", {
+      validatorErrors: {},
+      cumulativeFullAnswers: { establishment_type: "DOMESTIC" },
+      language: "en"
+    });
 
-    const $mainHeading = $("#main-heading");
-    expect($mainHeading.get(0).tagName).toEqual("h1");
+    const $mainHeading = $("h1");
+    expect($mainHeading.text().trim()).toEqual(
+      "Where is this establishment located?"
+    );
   });
 
   it("passes accessibility tests", async () => {
-    const $ = renderPage("establishment-address-type", "language: 'en'");
+    const $ = renderPage("establishment-address-type", {
+      validatorErrors: {},
+      cumulativeFullAnswers: { establishment_type: "DOMESTIC" },
+      language: "en"
+    });
 
     const results = await axe($.html());
     expect(results).toHaveNoViolations();
   });
 
-  it("renders 3 radio buttons with correct error props and default values", async () => {
-    const testValidatorErrors = {
-      example: "test error"
-    };
-
-    const $ = renderPage(
-      "establishment-address-type",
-      `language: 'en', validatorErrors:${testValidatorErrors}`
-    );
-
-    const $establishmentAddressTypeRadio = $("Radio");
-    expect(
-      $establishmentAddressTypeRadio._root[0].children[1].children.length
-    ).toBe(3);
-  });
-
-  it("renders the correct error", async () => {
-    const validatorErrors = {
-      example: "test error"
-    };
+  it("renders 3 radio buttons", async () => {
     const $ = renderPage("establishment-address-type", {
-      language: "cy",
-      validatorErrors
+      validatorErrors: {},
+      cumulativeFullAnswers: { establishment_type: "DOMESTIC" },
+      language: "en"
     });
 
-    const $establishmentAddressTypeRadio = $(".govuk-error-summary__list a");
-    expect($establishmentAddressTypeRadio.length()).toBe(1);
-    expect($establishmentAddressTypeRadio.get(0).text()).toBe("test error");
+    const $establishmentAddressTypeRadio = $(":radio");
+    expect($establishmentAddressTypeRadio.length).toBe(3);
   });
 
   describe("Radio boxes have correct value", () => {
-    it("Mobile radio has correct value", async () => {
+    it("renders the Mobile radio button with the correct value", () => {
       const $ = renderPage("establishment-address-type", "language: 'en'");
       const $mainHeadingMobile = $("#establishment_type_mobile_moveable");
       expect($mainHeadingMobile.get(0).attribs.value).toBe("MOBILE");
     });
-    it("Domestic radio has correct value", async () => {
+    it("renders the Domestic radio button with the correct value", () => {
       const $ = renderPage("establishment-address-type", "language: 'en'");
       const $mainHeadingDomestic = $("#establishment_type_home_domestic");
       expect($mainHeadingDomestic.get(0).attribs.value).toBe("DOMESTIC");
     });
-    it("Commercial radio has correct value", async () => {
+    it("renders the Commercial radio button with the correct value", () => {
       const $ = renderPage("establishment-address-type", "language: 'en'");
       const $mainHeadingCommercial = $(
         "#establishment_type_business_commercial"
       );
       expect($mainHeadingCommercial.get(0).attribs.value).toBe("COMMERCIAL");
     });
+  });
+
+  it("renders the correct error", async () => {
+    const $ = renderPage("establishment-address-type", {
+      language: "cy",
+      validatorErrors: {
+        establishment_type: "test error"
+      }
+    });
+
+    const $pageErrors = $(".govuk-error-summary__list a");
+    expect($pageErrors.length).toBe(1);
+    expect($pageErrors.contents().get(0).data).toBe("test error");
   });
 });
