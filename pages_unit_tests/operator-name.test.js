dce5f09ea1d60e8e633b99f2a66ad1a5ee83bc60
jkaur2303 <jaswinder.kaur@civica.co.uk>
2021-10-12 08:39:37 +0000

    added new function to wrap existing functions minor amendments to test page.

--- pages_unit_tests/operator-name.test.js
+++ pages_unit_tests/operator-name.test.js
@@ -1,10 +1,10 @@
-const { axe, renderPage } = require("../testHelpers");
+const { axe, renderPage, getPageDetails } = require("../testHelpers");
 
 const props = {
   validatorErrors: {},
   cumulativeFullAnswers: {
-    operator_first_name: "test name1",
-    operator_last_name: "test name2"
+    operator_first_name: "test name",
+    operator_last_name: "test lastname"
   },
   language: "en"
 };
@@ -18,23 +18,23 @@ describe("Operator-Name", () => {
 
   it("renders without crashing", () => {
     const $ = renderPage("operator-name", props);
-
-    const $mainHeading = $("h1");
+    const $mainHeading = getPageDetails.getMainHeading($);
     expect($mainHeading.text().trim()).toEqual("What is the operator's name?");
   });
 
-  describe("It should have correct input fields", () => {
+  describe("It should have correct input fields and value", () => {
     it("renders operator first name correctly", () => {
       const $ = renderPage("operator-name", props);
       const $firstname = $("#operator_first_name");
       expect($firstname.get(0).attribs.name).toBe("operator_first_name");
+      expect($firstname.get(0).attribs.value).toBe("test name");
     });
 
     it("renders operator last name correctly", () => {
-      const $ = renderPage("operator-name", "language: 'en'");
-      const $ = renderPage("establishment-address-type", props);
-      const $firstname = $("#operator_last_name");
-      expect($firstname.get(0).attribs.name).toBe("operator_last_name");
+      const $ = renderPage("operator-name", props);
+      const $lastname = $("#operator_last_name");
+      expect($lastname.get(0).attribs.name).toBe("operator_last_name");
+      expect($lastname.get(0).attribs.value).toBe("test lastname");
     });
   });
 
@@ -42,12 +42,12 @@ describe("Operator-Name", () => {
     const $ = renderPage("operator-name", {
       language: "cy",
       validatorErrors: {
-        operator_first_name: "test name1",
-        operator_last_name: "test name2"
+        operator_first_name: "test name",
+        operator_last_name: "test lastname"
       }
     });
 
-    const $pageErrors = $(".govuk-error-summary__list a");
+    const $pageErrors = getPageDetails.getErrorSummaryLinks($);
     expect($pageErrors.length).toBe(2);
     expect($pageErrors.contents().get(0).data).toBe("test name1");
     expect($pageErrors.contents().get(1).data).toBe("test name2");
