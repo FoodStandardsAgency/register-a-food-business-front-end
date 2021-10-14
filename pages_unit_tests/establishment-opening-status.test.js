9bc5f3215035c07a2ba746163d1c515ced2bde8a
David Kasali <david.kasali@civica.co.uk>
2021-10-14 09:46:05 +0000

    opening status test

--- pages_unit_tests/establishment-opening-status.test.js
+++ pages_unit_tests/establishment-opening-status.test.js
@@ -1,40 +1,78 @@
-const establishmentTradingName = require("../pages/establishment-opening-status.njk");
-const { axe, renderPage } = require("../testHelpers")
+const { axe, renderPage, getPageDetails } = require("../testHelpers")
+
+const props = {
+  validatorErrors: {},
+  cumulativeFullAnswers: { establishment_opening_status : "Establishment is already trading" },
+  language: "en"
+};
 
 describe("Establishment-Opening-Status", () => {
   it("renders without crashing", () => {
-    const $ = renderPage("establishment-opening-status", "language: 'en'");
+    const $ = renderPage("establishment-opening-status", props);
     
-    const $mainHeading = $('#main-heading')
-    expect($mainHeading.get(0).tagName).toEqual('h1')
+    const $mainHeading = getPageDetails.getMainHeading($)
+    expect($mainHeading.text().trim()).toEqual('Is this establishment already trading?')
   });
 
   it('passes accessibility tests', async () => {
-    const $ = renderPage('establishment-opening-status', "language: 'en'")
+    const $ = renderPage("establishment-opening-status", props);
 
     const results = await axe($.html())
     expect(results).toHaveNoViolations()
   })
 
-  it('Heading text correct', async () => {
-    const $ = renderPage('establishment-opening-status', "language: 'en'")
+  it("renders 3 radio buttons", async () => {
+    const $ = renderPage("establishment-opening-status", props);
 
-    const $mainHeading = $('#main-heading')
-    expect($mainHeading.get(0).children[0].data).toBe("Is this establishment already trading?")
-});
+    const $establishmentOpeningStatusRadios = getPageDetails.getRadioButtons($);
+    expect($establishmentOpeningStatusRadios.length).toBe(2);
+  });
 
-  describe('Radio boxes have correct value', () =>{
-    it('Already trading radio has correct value', async () => {
-      const $ = renderPage('establishment-opening-status', "language: 'en'")
-      const $mainHeadingTrading = $('#establishment_opening_status_already_trading')
-      expect($mainHeadingTrading.get(0).attribs.value).toBe("Establishment is already trading")
+  describe("Radio boxes have correct value", () => {
+    it("renders the Establishment is already trading radio button with the correct value", () => {
+      const $ = renderPage("establishment-opening-status", props);
+      const $mainHeadingMobile = $("#establishment_opening_status_already_trading");
+      expect($mainHeadingMobile.get(0).attribs.value).toBe("Establishment is already trading");
     });
-    it('Not trading radio has correct value', async () => {
-      const $ = renderPage('establishment-opening-status', "language: 'en'")
-      const $mainHeadingNotTrading = $('#establishment_opening_status_not_trading')
-      expect($mainHeadingNotTrading.get(0).attribs.value).toBe("Establishment due to trade")
+    it("renders the Establishment due to trade radio button with the correct value", () => {
+      const $ = renderPage("establishment-opening-status", props);
+      const $mainHeadingDomestic = $("#establishment_opening_status_not_trading");
+      expect($mainHeadingDomestic.get(0).attribs.value).toBe("Establishment due to trade");
     });
-  })
+    it("select the correct radio button based on session data", () => {
+      const $ = renderPage("establishment-opening-status", props);
+      const $selected = $("input:checked");
+      expect($selected.get(0).attribs.value).toBe("Establishment is already trading");
+    });
+  });
+
+  describe("Error messages displayed", () => {
+    it("renders the correct summary error", async () => {
+      const $ = renderPage("establishment-opening-status", {
+        language: "cy",
+        validatorErrors: {
+          establishment_opening_status: "test error"
+        }
+      });
+
+      const $pageErrors = getPageDetails.getErrorSummaryLinks($);
+      expect($pageErrors.length).toBe(1);
+      expect($pageErrors.contents().get(0).data).toBe("test error");
+    });
+
+    it("renders the correct error", async () => {
+      const $ = renderPage("establishment-opening-status", {
+        language: "cy",
+        validatorErrors: {
+          establishment_opening_status: "test error"
+        }
+      });
+
+      const $radioError = $("#establishment_opening_status-error");
+      expect($radioError.length).toBe(1);
+      expect($radioError.contents().get(2).data.trim()).toBe("test error");
+    });
+  });
 })
 
 
