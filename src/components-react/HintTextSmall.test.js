aa0008e6775ae504a44aa02622c424887e3eb58d
Simon Thomas <simon.thomas@civica.co.uk>
2022-01-10 15:35:45 +0000

    remove all react files

--- src/components-react/HintTextSmall.test.js
+++
@@ -1,9 +0,0 @@
-import HintTextSmall from "../components/HintTextSmall";
-import { shallow } from "enzyme";
-
-describe("<HintTextSmall  />", () => {
-  it("renders without crashing", () => {
-    const wrapper = shallow(<HintTextSmall>"It works"</HintTextSmall>);
-    expect(wrapper.length).toBe(1);
-  });
-});
