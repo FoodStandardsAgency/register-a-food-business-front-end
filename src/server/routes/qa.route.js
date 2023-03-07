37f748515530548bb58468df660000320b6ab2ff
Ernestas Leliuga <ernestas.leliuga@civica.co.uk>
2023-03-07 11:25:50 +0000

    add la_id to QA route

--- src/server/routes/qa.route.js
+++ src/server/routes/qa.route.js
@@ -7,35 +7,47 @@
 const { Router } = require("express");
 const { logEmitter } = require("../services/logging.service");
 const { QA_KEY } = require("../config");
+const {
+  getCouncilDataByID
+} = require("../connectors/config-db/config-db.connector");
 
 const qaRouter = () => {
   const router = Router();
 
-  router.get("/:target", (req, res) => {
+  router.get("/:target", async (req, res, next) => {
     logEmitter.emit("functionCall", "Routes", "/qa/:target route");
-    if (req.query.QA_KEY && req.query.QA_KEY === QA_KEY) {
-      req.session.council = req.params.lc;
-      const target = req.params.target;
-      delete req.query.QA_KEY;
-      req.session.cumulativeFullAnswers = req.query;
-      logEmitter.emit(
-        "functionSuccessWith",
-        "Routes",
-        "/qa/:target route",
-        target
-      );
-      req.session.save(() => {
-        res.redirect(`/new/${target}`);
-      });
-    } else {
-      logEmitter.emit(
-        "functionFail",
-        "Routes",
-        "/qa/:target route",
-        "403 not permitted"
-      );
-      res.status(403);
-      res.send("Not permitted");
+    try {
+      if (req.query.QA_KEY && req.query.QA_KEY === QA_KEY) {
+        if (req.query.la_id && !isNaN(req.query.la_id)) {
+          req.session.localAuthority = await getCouncilDataByID(
+            +req.query.la_id
+          );
+        }
+        const target = req.params.target;
+        delete req.query.QA_KEY;
+        req.session.cumulativeFullAnswers = req.query;
+        logEmitter.emit(
+          "functionSuccessWith",
+          "Routes",
+          "/qa/:target route",
+          target
+        );
+        req.session.save(() => {
+          res.redirect(`/new/${target}`);
+        });
+      } else {
+        logEmitter.emit(
+          "functionFail",
+          "Routes",
+          "/qa/:target route",
+          "403 not permitted"
+        );
+        res.status(403);
+        res.send("Not permitted");
+      }
+    } catch (err) {
+      logEmitter.emit("functionFail", "Routes", "/qa/:target route");
+      next(err);
     }
   });
 
