3c3bd54a927871d665c5ad0805fe22fac6e7ca09
Ernestas Leliuga <ernestas.leliuga@civica.co.uk>
2023-06-22 16:37:17 +0100

    Added unit test GA tag

--- pages_unit_tests/index.test.js
+++ pages_unit_tests/index.test.js
@@ -24,4 +24,12 @@ describe("Index", () => {
     const results = await axe($.html());
     expect(results).toHaveNoViolations();
   });
+  it("renders Google Analytic tag", async () => {
+    const $ = renderPage("index", props);
+
+    const results = await axe($.html());
+    expect($("script").get(0).attribs.src).toBe(
+      "https://www.googletagmanager.com/gtag/js?id=G-YCZ4TF8DJM"
+    );
+  });
 });
