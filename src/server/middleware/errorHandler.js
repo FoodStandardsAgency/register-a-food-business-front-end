7413a0920134aed7b2ce9902637c60f3fa254b5f
Simon Thomas <simon.thomas@civica.co.uk>
2024-04-15 14:04:44 +0100

    don't alert for postcoder error

--- src/server/middleware/errorHandler.js
+++ src/server/middleware/errorHandler.js
@@ -5,7 +5,12 @@ const errorHandler = (err, req, res, next) => {
   if (res.headersSent) {
     logEmitter.emit("error", `Application error: ${err && err.message}`); // Does not trigger Azure alert
     return next(err);
-  } else if (err.message == "invalid csrf token" || err.message.includes("Cannot read properties of undefined" || err.message.includes("template not found"))) {
+  } else if (
+    err.message == "invalid csrf token" ||
+    err.message.includes("Cannot read properties of undefined") ||
+    err.message.includes("template not found") ||
+    err.message.includes("fetch using postcoder failed")
+  ) {
     logEmitter.emit("error", `Application error: ${err && err.message}`); // Does not trigger Azure alert
   } else {
     logEmitter.emit("error", `Application error handled - ${err && err.message}`); // Used for Azure alerts
@@ -15,7 +20,7 @@ const errorHandler = (err, req, res, next) => {
   var props = {
     statusCode: res ? res.statusCode : err ? err.statusCode : "500",
     err: err ? err : "An error occurred."
-   };
+  };
   if (err.message.match("template not found")) {
     res.render("page-not-found", { props });
   } else {
