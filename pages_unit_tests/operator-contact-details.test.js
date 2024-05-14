ba60492f25020b75a9f6c2ba21fea90dbd92e4d6
Joe-Cun-Civica <joseph.cunningham@civica.co.uk>
2024-05-14 15:11:53 +0100

    fixed broken test

--- pages_unit_tests/operator-contact-details.test.js
+++ pages_unit_tests/operator-contact-details.test.js
@@ -26,7 +26,7 @@ describe("operator-contact-details", () => {
 
   it("renders without crashing", () => {
     const $mainHeading = getPageDetails.getMainHeading($);
-    expect($mainHeading.text().trim()).toEqual("Partnership contact details");
+    expect($mainHeading.text().trim()).toEqual("Partnership details");
   });
 
   it("renders correct inset text", () => {
@@ -52,13 +52,13 @@ describe("operator-contact-details", () => {
     });
 
     it("renders operator contact detail labels correctly", () => {
-      const $inputLabelTextMainNumber = getPageDetails.getInputLabelText($, 0);
+      const $inputLabelTextMainNumber = getPageDetails.getInputLabelText($, 3);
       expect($inputLabelTextMainNumber).toEqual("Main phone number");
 
-      const $inputLabelTextSecondaryNumber = getPageDetails.getInputLabelText($, 1);
+      const $inputLabelTextSecondaryNumber = getPageDetails.getInputLabelText($, 4);
       expect($inputLabelTextSecondaryNumber).toEqual("Secondary phone number (optional)");
 
-      const $inputLabelTextEmail = getPageDetails.getInputLabelText($, 2);
+      const $inputLabelTextEmail = getPageDetails.getInputLabelText($, 5);
       expect($inputLabelTextEmail).toEqual("Email address");
     });
   });
