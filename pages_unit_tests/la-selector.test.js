967a7472e862826a324b6e695c1fa93838d1df44
Joe-Cun-Civica <joseph.cunningham@civica.co.uk>
2024-08-19 16:00:07 +0100

    uncommented changes and fixed tests

--- pages_unit_tests/la-selector.test.js
+++ pages_unit_tests/la-selector.test.js
@@ -1,73 +1,69 @@
-describe("Sanity check", () => {
-  it("should pass if 1 equals 1", () => {
-    expect(1).toBe(1);
+/**
+ * @jest-environment jsdom
+ */
+
+const { axe, renderPage, getPageDetails } = require("../testHelpers");
+
+const props = {
+  validatorErrors: {},
+  cumulativeFullAnswers: {
+    local_authority: "8015"
+  },
+  localAuthorities: [
+    {
+      _id: 8015,
+      local_council: "Cardiff"
+    }
+  ],
+  fullCurrentPage: "",
+  language: "en"
+};
+
+describe("la-selector", () => {
+  it("renders without crashing", () => {
+    const $ = renderPage("la-selector", props);
+
+    const $mainHeading = getPageDetails.getMainHeading($);
+    expect($mainHeading.text().trim()).toEqual("We couldn't find your Local Authority");
+  });
+
+  it("passes accessibility tests", async () => {
+    const $ = renderPage("la-selector", props);
+
+    const results = await axe($.html());
+    expect(results).toHaveNoViolations();
+  });
+  it("shows the correct back link after wrong LA button is pressed on la-established page", async () => {
+    const props = {
+      currentPage: "/la-selector",
+      fullCurrentPage: "/la-selector?back=wrong-la"
+    };
+    const $ = renderPage("la-selector", props);
+
+    const $backlink = getPageDetails.getBacklinkHref($);
+    expect($backlink).toEqual("/new/la-established");
+  });
+
+  it("select the correct dropdown item based on session data", () => {
+    const $ = renderPage("la-selector", props);
+    const $selected = $(":selected");
+    expect($selected.contents().get(0).data).toBe("Cardiff");
+  });
+
+  describe("Error messages displayed", () => {
+    it("renders the correct summary error", async () => {
+      const $ = renderPage("la-selector", {
+        language: "cy",
+        validatorErrors: {
+          local_authority: "test error"
+        },
+        fullCurrentPage: ""
+      });
+
+      const $pageErrors = getPageDetails.getErrorSummaryLinks($);
+      expect($pageErrors.length).toBe(1);
+      expect($pageErrors.contents().get(0).data).toBe("test error");
+      expect($pageErrors.get(0).attribs.href).toBe("#local_authority");
+    });
   });
 });
-
-// /**
-//  * @jest-environment jsdom
-//  */
-
-// const { axe, renderPage, getPageDetails } = require("../testHelpers");
-
-// const props = {
-//   validatorErrors: {},
-//   cumulativeFullAnswers: {
-//     local_authority: "8015"
-//   },
-//   localAuthorities: [
-//     {
-//       _id: 8015,
-//       local_council: "Cardiff"
-//     }
-//   ],
-//   language: "en"
-// };
-
-// describe("la-selector", () => {
-//   it("renders without crashing", () => {
-//     const $ = renderPage("la-selector", props);
-
-//     const $mainHeading = getPageDetails.getMainHeading($);
-//     expect($mainHeading.text().trim()).toEqual("We couldn't find your Local Authority");
-//   });
-
-//   it("passes accessibility tests", async () => {
-//     const $ = renderPage("la-selector", props);
-
-//     const results = await axe($.html());
-//     expect(results).toHaveNoViolations();
-//   });
-//   it("shows the correct back link after wrong LA button is pressed on la-established page", async () => {
-//     const props = {
-//       currentPage: "/la-selector",
-//       fullCurrentPage: "/la-selector?back=wrong-la"
-//     };
-//     const $ = renderPage("la-selector", props);
-
-//     const $backlink = getPageDetails.getBacklinkHref($);
-//     expect($backlink).toEqual("/new/la-established");
-//   });
-
-//   it("select the correct dropdown item based on session data", () => {
-//     const $ = renderPage("la-selector", props);
-//     const $selected = $(":selected");
-//     expect($selected.contents().get(0).data).toBe("Cardiff");
-//   });
-
-//   describe("Error messages displayed", () => {
-//     it("renders the correct summary error", async () => {
-//       const $ = renderPage("la-selector", {
-//         language: "cy",
-//         validatorErrors: {
-//           local_authority: "test error"
-//         }
-//       });
-
-//       const $pageErrors = getPageDetails.getErrorSummaryLinks($);
-//       expect($pageErrors.length).toBe(1);
-//       expect($pageErrors.contents().get(0).data).toBe("test error");
-//       expect($pageErrors.get(0).attribs.href).toBe("#local_authority");
-//     });
-//   });
-// });
--- src/server/routes/find-local-authority.route.test.js
+++ src/server/routes/find-local-authority.route.test.js
@@ -1,196 +1,205 @@
-describe("Sanity check", () => {
-  it("should pass if 1 equals 1", () => {
-    expect(1).toBe(1);
+jest.mock("express", () => ({
+  Router: jest.fn(() => ({
+    post: jest.fn(),
+    get: jest.fn()
+  }))
+}));
+
+jest.mock("../controllers/find-local-authority.controller");
+const { fn } = require("moment");
+const findLocalAuthorityController = require("../controllers/find-local-authority.controller");
+const { findLocalAuthorityRouter } = require("./find-local-authority.route.js");
+
+describe("findLocalAuthority route: ", () => {
+  let router, handler;
+  beforeEach(() => {
+    router = findLocalAuthorityRouter();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("POST to /findlocalauthority/:originator", () => {
+    describe("When session.save works", () => {
+      const req = {
+        session: {
+          cumulativeFullAnswers: {},
+          addressLookups: {
+            establishment_postcode_find: []
+          },
+          save: (cb) => {
+            cb();
+          }
+        },
+        body: "body",
+        params: {
+          originator: "/some-page"
+        },
+        headers: {
+          referer: ""
+        }
+      };
+      const next = jest.fn();
+      const res = {
+        redirect: jest.fn()
+      };
+
+      beforeEach(() => {
+        findLocalAuthorityController.mockImplementation(() => ({
+          cumulativeFullAnswers: { example: "answer" },
+          validatorErrors: {},
+          localAuthority: {
+            local_council: "City of Cardiff Council",
+            local_council_url: "cardiff",
+            country: "wales"
+          },
+          redirectRoute: "/another-page"
+        }));
+        handler = router.post.mock.calls[0][1];
+        handler(req, res, next);
+      });
+      it("Should redirect to the redirectRoute page", () => {
+        expect(res.redirect).toBeCalledWith("/new/another-page");
+      });
+
+      it("Should set req.session.localAuthority.local_council_url", () => {
+        expect(req.session.localAuthority.local_council_url).toEqual("cardiff");
+      });
+
+      it("Should set req.session.localAuthority.country", () => {
+        expect(req.session.localAuthority.country).toEqual("wales");
+      });
+
+      it("Should set req.session.localAuthority.local_council", () => {
+        expect(req.session.localAuthority.local_council).toEqual("City of Cardiff Council");
+      });
+
+      it("Should update session", () => {
+        expect(req.session.cumulativeFullAnswers).toEqual({
+          example: "answer"
+        });
+        expect(req.session.validatorErrors).toEqual({});
+      });
+    });
+
+    describe("When local authority not onboarded", () => {
+      const req = {
+        session: {
+          cumulativeFullAnswers: {},
+          addressLookups: {
+            establishment_postcode_find: []
+          },
+          save: (cb) => {
+            cb();
+          }
+        },
+        body: "body",
+        params: {
+          originator: "/some-page"
+        },
+        headers: {
+          referer: ""
+        }
+      };
+      const next = jest.fn();
+      const res = {
+        redirect: jest.fn()
+      };
+
+      beforeEach(() => {
+        findLocalAuthorityController.mockImplementation(() => ({
+          cumulativeFullAnswers: { example: "answer" },
+          validatorErrors: {},
+          localAuthority: {
+            local_council: "City of Cardiff Council",
+            local_council_url: "cardiff",
+            country: "wales",
+            reg_form_url: "https://www.test.com"
+          },
+          redirectRoute: "/another-page"
+        }));
+        handler = router.post.mock.calls[0][1];
+        handler(req, res, next);
+      });
+      it("Should call redirect", () => {
+        expect(res.redirect).toBeCalledWith("https://www.test.com");
+      });
+    });
+
+    describe("When session.save throws an error", () => {
+      let response, next;
+      next = jest.fn();
+      const req = {
+        session: {
+          cumulativeFullAnswers: {},
+          addressLookups: {
+            establishment_postcode_find: []
+          },
+          save: (cb) => {
+            cb("session save error");
+          }
+        },
+        body: "body",
+        params: {
+          originator: "/some-page"
+        }
+      };
+      const res = {
+        redirect: jest.fn()
+      };
+
+      beforeEach(async () => {
+        findLocalAuthorityController.mockImplementation(() => ({
+          cumulativeFullAnswers: { example: "answer" },
+          validatorErrors: {},
+          localAuthority: "Cardiff",
+          redirectRoute: "/another-page"
+        }));
+        handler = router.post.mock.calls[0][1];
+        try {
+          await handler(req, res, next);
+        } catch (err) {
+          response = err;
+        }
+      });
+
+      it("Should throw an error", () => {
+        expect(next).toBeCalledWith("session save error");
+      });
+    });
+
+    describe("when an error is thrown", () => {
+      let next;
+      beforeEach(async () => {
+        findLocalAuthorityController.mockImplementation(() => {
+          throw new Error("error");
+        });
+        next = jest.fn();
+        handler = router.post.mock.calls[0][1];
+        req = {
+          session: {
+            addressLookups: {
+              establishment_postcode_find: []
+            },
+            cumulativeFullAnswers: {},
+            localAuthority: "Cardiff"
+          },
+          body: "body",
+          params: {
+            originator: "/some-page"
+          }
+        };
+
+        res = {
+          redirect: jest.fn()
+        };
+
+        handler(req, res, next);
+      });
+      it("should call next with error", () => {
+        expect(next).toBeCalledWith(new Error("error"));
+      });
+    });
   });
 });
-
-// jest.mock("express", () => ({
-//   Router: jest.fn(() => ({
-//     post: jest.fn(),
-//     get: jest.fn()
-//   }))
-// }));
-
-// jest.mock("../controllers/find-local-authority.controller");
-// const findLocalAuthorityController = require("../controllers/find-local-authority.controller");
-// const { findLocalAuthorityRouter } = require("./find-local-authority.route.js");
-
-// describe("findLocalAuthority route: ", () => {
-//   let router, handler;
-//   beforeEach(() => {
-//     router = findLocalAuthorityRouter();
-//   });
-
-//   afterEach(() => {
-//     jest.clearAllMocks();
-//   });
-
-//   describe("POST to /findlocalauthority/:originator", () => {
-//     describe("When session.save works", () => {
-//       const req = {
-//         session: {
-//           cumulativeFullAnswers: {},
-//           save: (cb) => {
-//             cb();
-//           }
-//         },
-//         body: "body",
-//         params: {
-//           originator: "/some-page"
-//         },
-//         headers: {
-//           referer: ""
-//         }
-//       };
-//       const res = {
-//         redirect: jest.fn()
-//       };
-
-//       beforeEach(() => {
-//         findLocalAuthorityController.mockImplementation(() => ({
-//           cumulativeFullAnswers: { example: "answer" },
-//           validatorErrors: {},
-//           localAuthority: {
-//             local_council: "City of Cardiff Council",
-//             local_council_url: "cardiff",
-//             country: "wales"
-//           },
-//           redirectRoute: "/another-page"
-//         }));
-//         handler = router.post.mock.calls[0][1];
-//         handler(req, res);
-//       });
-//       it("Should redirect to the redirectRoute page", () => {
-//         expect(res.redirect).toBeCalledWith("/new/another-page");
-//       });
-
-//       it("Should set req.session.localAuthority.local_council_url", () => {
-//         expect(req.session.localAuthority.local_council_url).toEqual("cardiff");
-//       });
-
-//       it("Should set req.session.localAuthority.country", () => {
-//         expect(req.session.localAuthority.country).toEqual("wales");
-//       });
-
-//       it("Should set req.session.localAuthority.local_council", () => {
-//         expect(req.session.localAuthority.local_council).toEqual("City of Cardiff Council");
-//       });
-
-//       it("Should update session", () => {
-//         expect(req.session.cumulativeFullAnswers).toEqual({
-//           example: "answer"
-//         });
-//         expect(req.session.validatorErrors).toEqual({});
-//       });
-//     });
-
-//     describe("When local authority not onboarded", () => {
-//       const req = {
-//         session: {
-//           cumulativeFullAnswers: {},
-//           save: (cb) => {
-//             cb();
-//           }
-//         },
-//         body: "body",
-//         params: {
-//           originator: "/some-page"
-//         },
-//         headers: {
-//           referer: ""
-//         }
-//       };
-//       const res = {
-//         redirect: jest.fn()
-//       };
-
-//       beforeEach(() => {
-//         findLocalAuthorityController.mockImplementation(() => ({
-//           cumulativeFullAnswers: { example: "answer" },
-//           validatorErrors: {},
-//           localAuthority: {
-//             local_council: "City of Cardiff Council",
-//             local_council_url: "cardiff",
-//             country: "wales",
-//             reg_form_url: "https://www.test.com"
-//           },
-//           redirectRoute: "/another-page"
-//         }));
-//         handler = router.post.mock.calls[0][1];
-//         handler(req, res);
-//       });
-//       it("Should call redirect", () => {
-//         expect(res.redirect).toBeCalledWith("https://www.test.com");
-//       });
-//     });
-
-//     describe("When session.save throws an error", () => {
-//       let response, next;
-//       next = jest.fn();
-//       const req = {
-//         session: {
-//           cumulativeFullAnswers: {},
-//           save: (cb) => {
-//             cb("session save error");
-//           }
-//         },
-//         body: "body",
-//         params: {
-//           originator: "/some-page"
-//         }
-//       };
-//       const res = {
-//         redirect: jest.fn()
-//       };
-
-//       beforeEach(async () => {
-//         findLocalAuthorityController.mockImplementation(() => ({
-//           cumulativeFullAnswers: { example: "answer" },
-//           validatorErrors: {},
-//           localAuthority: "Cardiff",
-//           redirectRoute: "/another-page"
-//         }));
-//         handler = router.post.mock.calls[0][1];
-//         try {
-//           await handler(req, res, next);
-//         } catch (err) {
-//           response = err;
-//         }
-//       });
-
-//       it("Should throw an error", () => {
-//         expect(next).toBeCalledWith("session save error");
-//       });
-//     });
-
-//     describe("when an error is thrown", () => {
-//       let next;
-//       beforeEach(async () => {
-//         findLocalAuthorityController.mockImplementation(() => {
-//           throw new Error("error");
-//         });
-//         next = jest.fn();
-//         handler = router.post.mock.calls[0][1];
-//         req = {
-//           session: {
-//             cumulativeFullAnswers: {},
-//             localAuthority: "Cardiff"
-//           },
-//           body: "body",
-//           params: {
-//             originator: "/some-page"
-//           }
-//         };
-
-//         res = {
-//           redirect: jest.fn()
-//         };
-
-//         handler(req, res, next);
-//       });
-//       it("should call next with error", () => {
-//         expect(next).toBeCalledWith(new Error("error"));
-//       });
-//     });
-//   });
-// });
